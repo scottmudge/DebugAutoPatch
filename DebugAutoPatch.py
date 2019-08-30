b9ab9c2af2eaf4437b9b8aeec83f36064065bfc9
Scott Mudge <mail@scottmudge.com>
2019-08-29 20:26:00 -0400

    Initial commit.

--- DebugAutoPatch.py
+++ DebugAutoPatch.py
@@ -1,7 +1,32 @@
+import logging
 import idaapi
 
 
 class DebugAutoPatchPlugin(idaapi.plugin_t):
     flags = 0
     comment = "Plugin for automatic patch injection - no file patching needed!"
-    help = "See "
+    help = "See https://github.com/scottmudge/IDA_DebugAutoPatch/blob/master/readme.md"
+    wanted_name = "DebugAutoPatch"
+    wanted_hotkey = ""
+
+    @staticmethod
+    def init():
+        pass
+
+    @staticmethod
+    def run():
+        pass
+
+    @staticmethod
+    def term():
+        pass
+
+
+def PLUGIN_ENTRY():
+    # load settings
+    logging.basicConfig(format='[%(levelname)s] %(message)s\t(%(module)s:%(funcName)s)')
+    logging.root.setLevel(logging.DEBUG)
+    #idaapi.notify_when(idaapi.NW_OPENIDB, cache.initialize_cache)
+    return DebugAutoPatchPlugin()
+
+
